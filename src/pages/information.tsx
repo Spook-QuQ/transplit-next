9361464de7158764f6a44477d510db3c2b123f83
spook-QuQ <59325698+spook-QuQ@users.noreply.github.com>
2022-10-26 11:56:44 +0900

    Made the information in detail

--- src/pages/information.tsx
+++ src/pages/information.tsx
@@ -1,28 +1,38 @@
 import { NextComponentType, NextPageContext } from 'next'
+import Head from 'next/head'
 
-import Link from 'next/link'
+// import Link from 'next/link'
 import { useRouter, NextRouter } from 'next/router'
 
-const Information: (NextComponentType<NextPageContext>) = ({  }) => {
+const Information: NextComponentType<NextPageContext> = ({}) => {
   // const router: NextRouter = useRouter()
   // console.log(router)
-  
+
   return (
-    <div>
-      <h3
-        className="
-          text-4xl
-          bg-slate-300
-          text-sky-900
-          font-semibold
-          font-serif
-        "
-      >Information{
-        // router.pathname
-      }</h3>
-      <Link href='/'>
-        <a>Home</a>
-      </Link>
+    <div className='flex flex-col items-center'>
+      <Head>
+        <title>Information - Transplit</title>
+        <meta name='description' content='Transplit' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <small className='pt-8'>Made by</small>
+      <h3>
+        {/* <span
+          className='
+            text-4xl
+            // bg-slate-300
+            // text-sky-900
+            font-semibold
+            font-serif
+          '
+        >
+          Information
+        </span> */}
+        Tomoaki Ohta
+        {
+          // router.pathname
+        }
+      </h3>
     </div>
   )
 }
